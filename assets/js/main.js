968ea10ae43151b552c663a0f24d9f1b49f6856e
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-04-04 14:57:00 +0200

    scomparsa numeri ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 let vai = document.querySelector("#vai")
+let gioco = document.querySelector("#gioco")
+let ready = document.querySelector(".ready")
 
 
 function generaNumeri() {
@@ -12,14 +14,40 @@ function generaNumeri() {
             array.push (num)
         }      
     }
-    
     return array
 }
 
-vai.addEventListener("click", function(){
-    generaNumeri()
-    let numerirandom = generaNumeri()
+function stampanumeri (array) {
+    let elemento = document.createElement( "div" )
+    elemento.classList.add ("card")
+    elemento.innerText = array
+    console.log (elemento)
+    return gioco.append(elemento)
+}
 
 
 
-})
+
+ vai.addEventListener("click", function(){
+     generaNumeri()
+
+    let numerirandom = generaNumeri()  
+    console.log(numerirandom)
+
+    for(i=0;i<=numerirandom.length-1;i++){
+
+        stampanumeri(numerirandom[i])
+    }
+
+    ready.classList.add("none")
+
+    setTimeout (function(){
+        
+        gioco.classList.add("none")
+        gioco.classList.remove("flex")
+
+    },5000)
+    
+
+
+ })
